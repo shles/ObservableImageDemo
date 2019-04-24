da8160efe5450c35bfae95fb0ce712d70ae49ba5
shlesberg <temitrix@gmail.com>
2019-04-24 15:12:41 +0300

    initial commit with pods

---
+++ ObservableImageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObservableImageDemo
+//
+//  Created by Артeмий Шлесберг on 24/04/2019.
+//  Copyright © 2019 Shlesberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+target 'ObservableImageDemo' do
+	use_frameworks!
+
+	pod 'RxSwift'
+end
